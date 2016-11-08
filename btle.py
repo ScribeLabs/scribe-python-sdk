c3d5759344c73dadc4402b1132aa19970a3208e9
nisu101 <jignesh@infocusp.in>
2016-11-08 19:26:28 +0530

    Implemented function for other operation

--- btle.py
+++ btle.py
@@ -8,6 +8,7 @@ from collections import OrderedDict as OD
 import Adafruit_BluefruitLE
 from Adafruit_BluefruitLE.services.servicebase import ServiceBase
 
+
 SERVICE_UUID = uuid.UUID("00001723-0000-1000-8000-00805f9b34fb") # 16-bit UUID "1723"
 RS_SERV_UUID = uuid.UUID("00001723-1212-EFDE-1523-785FEABCD123")
 # FIRMWARE_UPDATE_SERVICE_UUID = uuid.UUID("00001530-1212-efde-1523-785feabcd123")
@@ -31,6 +32,7 @@ class RunScribeDevice(ServiceBase):
         self.file_read = False
 
     def _data_received(self, data):
+        print "In data received : " + data[0] 
         if data[0] == "D":
             self._update_fs(data)
         elif data[0] == "R":
@@ -120,13 +122,22 @@ class RunScribeDevice(ServiceBase):
         r, g, b = struct.unpack(">BBB", resp[1:4])
         return (r, g, b)
 
+    def set_led_color(self):
+        self.write_packet("J" + chr(255) + chr(0) + chr(0))
+        resp = self._responses["J"].get()
+        r, g, b = struct.unpack(">BBB", resp[1:4])
+        return (r, g, b)        
+
     def read_data(self, file_list_index, file_point_reg, file_block_size=16):
         self.write_packet("R" + struct.pack(">BBI", file_list_index, file_block_size, file_point_reg))
 
     def _update_read(self, resp):
         point = ord(resp[1]) << 16 + ord(resp[2]) << 8 + ord(resp[3])
         data = resp[4:20]
-        print "data: ", repr(data)
+        f = open('temp.fit', 'a')
+        #print "data: ", repr(data)
+        f.write(repr(data))
+        f.close()
 
     def status(self):
         self.write_packet("S")
@@ -155,6 +166,12 @@ class RunScribeDevice(ServiceBase):
         return OD(zip(["version major", "version minor", "system time", "last access time", "last boot time",
                        "time since last boot"], struct.unpack(">BBIIII", resp[1:19])))
 
+    # TODO: Don't know what is device_time_*
+    def set_time(self, device_time_0, device_time_1, device_time_2, device_time_3):
+        self.write_packet("T" + struct.pack(">IIII", device_time_0, device_time_1, device_time_2, device_time_3))
+        resp = self._responses["T"].get()
+        return OD(zip(["version_major", "version_minor"], struct.unpack(">BB", resp[1:3])))
+
     def pooling_status(self):
         self.write_packet("N")
         resp = self._responses["N"].get()
@@ -165,6 +182,104 @@ class RunScribeDevice(ServiceBase):
         resp = self._responses["E"].get()
         return OD(zip(["version major", "version minor", "erase flash result", "file total size", "file point reg",
                          "crc high", "crc low"], struct.unpack(">BBBIIBB", resp[1:14])))
+
+    def manufacturing_mode(self, state=0): # 0 - off, 1 - on
+        #if state==0 :
+        #    self.write_packet("H" + struct.pack(">B", state))
+        self.write_packet("H" + struct.pack(">B", state))
+        #else :
+        #    self.write_packet("H\x01")
+        print 'written packet'
+        resp = self._responses["H"].get()
+        return OD(zip(["version major", "version minor"], struct.unpack(">BB", resp[1:3])))
+
+    def DFU_mode(self):
+        self.write_packet("F")
+
+    def set_mode(self, command=0, state=1):
+        self.write_packet("M" + struct.pack(">BB", command, state))
+        resp = self._responses["M"].get()
+        return OD(zip(["version major", "version minor"], struct.unpack(">BB", resp[1:3])))
+
+    def real_time_polling(self, mode): # 0 => Accel, 1 => Gyro, 2 => Compass
+        if mode == 0:
+            print "Hello accel"
+            self.write_packet("G\x80")
+            print "write packet"
+            resp = self._responses["G"].get()
+            print "return data"
+            return OD(zip(["accel 0", "accel 1", "accel 2", "accel 3", "accel 4",
+                            "accel 5", "quat 0", "quat 1", "quat 2", "quat 3", "quat 4", "quat 5",
+                            "quat 6", "quat 7"], struct.unpack("BBBBBBBBBBBBBB", resp[1:15])))
+        elif mode == 1:
+            self.write_packet("G\x81")
+            resp = self._responses["G"].get()
+            return OD(zip(["MPU data index", "gyro 0", "gyro 1", "gyro 2", "gyro 3", "gyro 4",
+                            "gyro 5", "quat 0", "quat 1", "quat 2", "quat 3", "quat 4", "quat 5",
+                            "quat 6", "quat 7"], struct.unpack("BBBBBBBBBBBBBBB", resp[1:16])))
+
+        else:
+            self.write_packet("G\x82")
+            resp = self._responses["G"].get()
+            return OD(zip(["MPU data index", "compass 0", "compass 1", "compass 2", "compass 3", "compass 4",
+                            "compass 5", "quat 0", "quat 1", "quat 2", "quat 3", "quat 4", "quat 5",
+                            "quat 6", "quat 7"], struct.unpack("BBBBBBBBBBBBBBB", resp[1:16])))
+
+    def light_led(self):
+        self.write_packet("L" + chr(13) + "\x01\x00\x00\xFF")
+        resp = self._responses["L"].get()
+        return OD(zip(["Mode", "cycles", "R" , "G", "B"],struct.unpack("BBBBB", resp[1:6])))
+
+    def read_config_data(self, configblocksize=16, configpoint=0):
+        self.write_packet("U" + struct.pack(">BH", configblocksize, configpoint))
+        resp = self._responses["U"].get()
+        if configblocksize == 16:
+            if configpoint == 0xAA01 or configpoint == 0xAA03:
+                return OD(zip(["config block size", "config point buf", "ble device 0", "ble device 1",
+                               "ble device 2", "ble device 3", "ble device 4", "ble device 5", "ble device 6",
+                               "ble device 7", "ble device 8", "ble device 9", "ble device 10", "ble device 11",
+                               "ble device 12", "ble device 13", "ble device 14", "ble device 15", "ble device 16"],
+                                struct.unpack("HBBBBBBBBBBBBBBBBB", resp[1:20])))
+            elif configpoint == 0xAA02:
+                return OD(zip(["config block size", "config point buf", "ble device led color", "heel/lace",
+                               "right/left", "device time 0", "device time 1", "device time 2", "device time 3",
+                               "device sample rate", "sensitivity", "timeout", "stride rate", "min conn interval",
+                               "max conn interval", "ble slave latency"], 
+                               struct.unpack("HBBBBBBBBBBBBBBB", resp[1:18])))
+            elif configpoint == 0xAA04:
+                return OD(zip(["config block size", "config point buf", "heel/lace", "right/left", "timeout", "stride rate",
+                            "scale factor A", "scale factor B", "min recording voltage MSB", "min recording voltage LSB",
+                            "deep sleep voltage MSB", "deep sleep voltage LSB", "R", "G", "B" ],
+                            struct.unpack("HBBBBBBBBBBBBBB",resp[1:17])))
+            elif configpoint == 0xAA05:
+                return OD(zip(["config block size", "config point buf", "min conn interval", "max conn interval", "ble slave latency"],
+                               struct.unpack("HBBBB",resp[1:7])))
+            elif configpoint == 0xAA06:
+                return OD(zip(["config block size", "config point buf", "raw data"], struct.unpack("HBB", resp[1:5])))
+            elif configpoint == 0xAA07:
+                return OD(zip(["config block size", "config point buf", "battery capacity"], struct.unpack("HBB", resp[1:5])))
+            else:
+                return OD(zip(["default","config point buf"], struct.unpack("HB", resp[1:4])))
+
+    def write_config_data(self, packet):
+        self.write_packet("V" + packet)
+        resp = self._responses["V"].get()
+        return OD(zip(["config block size", "config point buf"], struct.unpack("HB", resp[1:4])))
+
+    def perform_diagnostics(self):
+        self.write_packet("X\x00")
+        resp = self._responses["X"].get()
+        return OD(zip(["version major", "version minor", "diagnostics status", "diagnostics result"],struct.unpack("BBBB", resp[1:5])))
+
+    def get_diagnostics_results(self, packetOffset=0):
+        self.write_packet("Z" + chr(packetOffset))
+        resp = self._responses["Z"].get()
+        return OD(zip(["Total packets", "Packet Offset", "buffer chart 0", "buffer chart 1", 
+                       "buffer chart 2", "buffer chart 3", "buffer chart 4", "buffer chart 5",
+                       "buffer chart 6", "buffer chart 7", "buffer chart 8", "buffer chart 9", 
+                       "buffer chart 10", "buffer chart 11", "buffer chart 12", "buffer chart 13",
+                       "buffer chart 14"], struct.unpack("BBBBBBBBBBBBBBBBB", resp[1:18])))
+
 
 # Thread-Safe interface to bluetooth module
 class BTLE:
--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 
 from btle import BTLE
 import threading
+from Adafruit_BluefruitLE.services import DeviceInformation
+import struct
 
 def print_dict(dict):
     for key in dict:
@@ -15,7 +17,7 @@ def main(bt):
         for device in current_devices:
             if not device in devices:
                 devices.append(device)
-                print "#{}: {}".format(devices.index(device), device._peripheral.name())
+                print "#{}: {} {} {} {}".format(devices.index(device), device.name, device.id, device.is_connected, device.advertised)
     print "Type number of device + enter to connect"
     bt.scan(update_list)
 
@@ -23,53 +25,197 @@ def main(bt):
     device = devices[name]
     scribe = bt.connect_to(device)
     print ("Connected")
+    print "#{}: {} {} {} {}".format(devices.index(device), device.name, device.id, device.is_connected, device.advertised)
 
     while True:
         cmd = raw_input("> ")
         if cmd == "q":
             break
-        elif cmd == "read time":
-            print_dict(scribe.read_time())
-        elif cmd == "load file list":
-            files = scribe.load_file_list()
-            for f in files:
-                print_dict(f)
-        elif cmd == "status":
-            print_dict(scribe.status())
-        elif cmd == "read data":
-            print "Type file index : "
-            file_idx = int(raw_input())
-            print "Type file block data point : "
-            data_pt = int(raw_input())
-            scribe.read_data(file_idx, data_pt)
-        elif cmd == "stop read data":
-            print_dict(scribe.stop_read_data())
         elif cmd == "reboot":
             print "Reboot type : 0 - soft, 1 - hard"
             r_type = int(raw_input())
             scribe.reboot(r_type)
-        elif cmd == "pooling status":
-            print_dict(scribe.pooling_status())
-        elif cmd == "file information":
-            print "Type file index : "
-            file_idx = int(raw_input())
-            print_dict(scribe.get_file_info(file_idx))
-        elif cmd == "erase data":
-            print "erase type (1-partial 0,2-chip, 3-deleted, 4-EEPROM/default ): "
-            e_type = int(raw_input())
-            print "erase bit mask (1-file): "
-            e_bit_mask = int(raw_input())
-            print_dict(scribe.erase_data(e_type, e_bit_mask))
+        #elif cmd == "annotate file":
+        #    print_dict(scribe.annotate_file(0, 1))
         elif cmd == "update crc":
             print "file block size : (16-default)"
             file_block_size = int(raw_input())
             print "file point buffer : (0 - default)"
             file_point_buffer = int(raw_input())
             print_dict(scribe.update_crc_checksum(file_block_size, file_point_buffer))
-        elif cmd == "get led color":
-            scribe.get_led_color()
+        elif cmd == "load file list":
+            files = scribe.load_file_list()
+            for f in files:
+                print_dict(f)
+        elif cmd == "erase data":
+            print "erase type (1-partial 0,2-chip, 3-deleted, 4-EEPROM/default ): "
+            e_type = int(raw_input())
+            print "erase bit mask (1-file): "
+            e_bit_mask = int(raw_input())
+            print_dict(scribe.erase_data(e_type, e_bit_mask))
+        elif cmd == "DFU mode":
+            scribe.DFU_mode()
+            print "Scribe in DFU mode"
+        #elif cmd == "real time polling":
+        #    print "Enter mode (0 => Accel, 1 => Gyro, 2 => Compass):"
+        #    mode = int(raw_input())
+        #    print_dict(scribe.real_time_polling(mode))
+        #elif cmd == "manufacturing mode": # deep motion sleep
+        #    print "state : (0 - off, 1 - on)"
+        #    state = int(raw_input())
+        #    print_dict(scribe.manufacturing_mode(state))
+        elif cmd == "file information":
+            print "Type file index : "
+            file_idx = int(raw_input())
+            print_dict(scribe.get_file_info(file_idx))
+        #elif cmd == "set led color":
+        #    r, g, b = scribe.set_led_color()
+        #    string = 'Red : ' + str(r) + ', Green: ' + str(g) + ', Blue: ' + str(b)
+        #    print string
+        #elif cmd == "get led color":
+        #    r, g, b = scribe.get_led_color()
+        #    string = 'Red : ' + str(r) + 'Green: ' + str(g) + 'Blue: ' + str(b)
+        #    print string
         elif cmd == "light":
-            scribe.write_packet("L" + chr(13) + "\x01\x00\xff\x00")
+            print_dict(scribe.light_led())
+        elif cmd == "set mode":
+            print "Command (0 - N/A, 1 - record, 2 - pause, 3 - sync) : "
+            command = int(raw_input())
+            print "state : (0 - off, 1 - on)"
+            state = int(raw_input())
+            print_dict(scribe.set_mode(command, state))
+        elif cmd == "pooling status":
+            print_dict(scribe.pooling_status())
+        elif cmd == "stop read data":
+            print_dict(scribe.stop_read_data())
+        elif cmd == "read data":
+            print "Type file index : "
+            file_idx = int(raw_input())
+            print "Type file block data point : "
+            data_pt = int(raw_input())
+            scribe.read_data(file_idx, data_pt)
+        elif cmd == "status":
+            print_dict(scribe.status())
+        elif cmd == "set time":
+            print_dict(scribe.set_time(805306370, 1, 1, 2))
+        elif cmd == "read time":
+            print_dict(scribe.read_time())
+        elif cmd == "read configuration data":
+            print "Enter config point (0xAA01..7): "
+            configpoint = int(raw_input(),16)
+            print_dict(scribe.read_config_data(configblocksize = 16, configpoint = configpoint))
+        elif cmd == "write configuration data":
+            print "Enter config point (0xAA01..7))"
+            configpoint = int(raw_input(),16)
+            if configpoint == 0xAA01 or configpoint == 0xAA03:
+                print "config block size : "
+                configblocksize = int(raw_input())
+                for i in range(0,17) :
+                    print "ble device " + str(i) + " : "
+                    bledevice[i] = int(raw_input())
+                packet = struct.pack(">HBBBBBBBBBBBBBBBBB", configblocksize, 1, bledevice[0], bledevice[1], bledevice[2],
+                        bledevice[3], bledevice[4], bledevice[5], bledevice[6], bledevice[7], bledevice[8], bledevice[9], bledevice[10],
+                        bledevice[11], bledevice[12], bledevice[13], bledevice[14], bledevice[15], bledevice[16])
+            elif configpoint == 0xAA02:
+                print "config block size : "
+                configblocksize = int(raw_input())
+                print "led color : "
+                bledevice_ledcolor = int(raw_input())
+                print "heel/lace : "
+                heel_lace = int(raw_input())
+                print "right/left : "
+                right_left = int(raw_input())
+                for i in range(0,4):
+                    print "device time " + str(i) + " : "
+                    device_time[i] = int(raw_input())
+                print "device sample rate : "
+                device_sample_rate = int(raw_input())
+                print "sensitivity : "
+                sensitivity = int(raw_input())
+                print "Timeout : "
+                timeout = int(raw_input())
+                print "stride rate : "
+                stride_rate = int(raw_input())
+                print "min conn interval : "
+                min_conn_interval = int(raw_input())
+                print "max conn interval : "
+                max_conn_interval = int(raw_input())
+                print "ble slave latency : "
+                ble_slave_latency = int(raw_input())
+                packet = struct.pack(">HBBBBBBBBBBBBBBB", configblocksize, 2, bledevice_ledcolor, heel_lace,
+                               right_left, device_time[0], device_time[1], device_time[2], device_time[3], device_sample_rate, 
+                               sensitivity, timeout, stride_rate, min_conn_interval, max_conn_interval, ble_slave_latency)
+            elif configpoint == 0xAA04:
+                print "config block size : "
+                configblocksize = int(raw_input())
+                print "heel/lace : "
+                heel_lace = int(raw_input())
+                print "right/left : "
+                right_left = int(raw_input())
+                print "Timeout : "
+                timeout = int(raw_input())
+                print "stride rate : "
+                stride_rate = int(raw_input())
+                print "scale factor A : "
+                scale_factor_A = int(raw_input())
+                print "scale factor B : "
+                stride_rate = int(raw_input())
+                print "min recording voltage MSB : "
+                min_recording_voltage_MSB = int(raw_input())
+                print "min recording voltage LSB : "
+                min_recording_voltage_LSB = int(raw_input())
+                print "deep sleep voltage MSB : "
+                deep_sleep_voltage_MSB = int(raw_input())
+                print "deep sleep voltage LSB : "
+                deep_sleep_voltage_LSB = int(raw_input())
+                print "R : "
+                R = int(raw_input())
+                print "G : "
+                G = int(raw_input())
+                print "B : "
+                B = int(raw_input())
+                packet = struct.pack(">HBBBBBBBBBBBBBB", configblocksize, 4, heel_lace, right_left, timeout,
+                    stride_rate, scale_factor_A, scale_factor_B, min_recording_voltage_MSB, min_recording_voltage_LSB,
+                    deep_sleep_voltage_MSB, deep_sleep_voltage_LSB, R, G, B )
+            elif configpoint == 0xAA05:
+                print "config block size : "
+                configblocksize = int(raw_input())
+                print "min conn interval : "
+                min_conn_interval = int(raw_input())
+                print "max conn interval : "
+                max_conn_interval = int(raw_input())
+                print "ble slave latency : "
+                ble_slave_latency = int(raw_input())
+                packet = struct.pack(">HBBBB", configblocksize, 5, min_conn_interval, max_conn_interval, ble_slave_latency)
+            elif configpoint == 0xAA06:
+                print "config block size : "
+                configblocksize = int(raw_input())
+                print "raw data : "
+                raw_data = int(raw_input())
+                packet = struct.pack(">HBB", configblocksize, 6, raw_data)
+            elif configpoint == 0xAA07:
+                print "config block size : "
+                configblocksize = int(raw_input())
+                print "bettery capacity : "
+                bettery_capacity = int(raw_input())
+                packet = struct.pack(">BHB", configblocksize, 7, bettery_capacity)
+            print_dict(scribe.write_config_data(packet))
+        elif cmd == "perform diagnostics":
+            print_dict(scribe.perform_diagnostics())
+        elif cmd == "get diagnostics results":
+            print_dict(scribe.get_diagnostics_results())
+        elif cmd == "device information service":
+            dis = DeviceInformation(device)
+            # Print out the DIS characteristics.
+            print('Manufacturer: {0}'.format(dis.manufacturer))
+            print('Model: {0}'.format(dis.model))
+            print('Serial: {0}'.format(dis.serial))
+            print('Hardware Revision: {0}'.format(dis.hw_revision))
+            print('Software Revision: {0}'.format(dis.sw_revision))
+            print('Firmware Revision: {0}'.format(dis.fw_revision))
+            print('System ID: {0}'.format(dis.system_id))
+            print('Regulatory Cert: {0}'.format(dis.regulatory_cert))
+            print('PnP ID: {0}'.format(dis.pnp_id))
     bt.end()
 
 bt = BTLE()
